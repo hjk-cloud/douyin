ec3317a8eae73f3eba4a5ee40a324b23ab2ce579
weijianshu112139 <72077523+weijianshu112139@users.noreply.github.com>
2022-06-05 15:18:01 +0800

    Add files via upload

--- controller/feed.go
+++ controller/feed.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/hjk-cloud/douyin/models"
 	"github.com/hjk-cloud/douyin/service"
@@ -15,28 +16,38 @@ type FeedResponse struct {
 	NextTime  int64           `json:"next_time,omitempty"`
 }
 
+// Feed @Feed
+// @Description Feed
+// @Accept  json
+// @Produce json
+// @Param   name     path    string     true        "name"
+// @Success 200 {string} string	"name,helloWorld"
+// @Router /api/v1/sayHello/{name} [get]
 func Feed(c *gin.Context) {
 	token, flag := c.GetQuery("token")
 	timeStamp := c.Query("latest_time")
 
 	var latestTime time.Time
+	fmt.Println("feed.go : Timestamp ", timeStamp)
 	times, err := strconv.ParseInt(timeStamp, 10, 64)
+	fmt.Println("feed.go : times ", times, "date : Times ", time.Unix(times/1000, 0))
 	if err == nil {
-		latestTime = time.Unix(0, times*1e6)
+		latestTime = time.Unix(0, times*1e6).Local()
 	}
 	var videos []*models.Video
+
 	if !flag {
 		token = "-"
 	}
-
-	videos, err = service.VideoListWithToken(token, latestTime)
+	var nextTime int64
+	videos, err, nextTime = service.VideoListWithToken(token, latestTime)
 
 	//fmt.Println("controller ----------", videos)
 	if err == nil {
 		c.JSON(http.StatusOK, FeedResponse{
 			Response:  Response{StatusCode: 0},
 			VideoList: videos,
-			NextTime:  time.Now().Unix(),
+			NextTime:  nextTime,
 		})
 	} else {
 		c.JSON(http.StatusOK, FeedResponse{
