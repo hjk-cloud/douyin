1fb439e8e61c603f41f91289e1f902a40e8731f0
王硕 <1844664313@qq.com>
2022-06-08 09:39:57 +0800

    王硕---------redis测试，但不知道怎么部署

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/hjk-cloud/douyin/models"
 	"github.com/hjk-cloud/douyin/routers"
+	"github.com/hjk-cloud/douyin/util"
 )
 
 func main() {
@@ -12,6 +13,9 @@ func main() {
 	r := gin.Default()
 
 	routers.InitRouter(r)
+
+	//用来测试redis连接
+	util.GetAll()
 
 	r.Run()
 }
